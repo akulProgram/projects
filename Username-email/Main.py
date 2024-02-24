e8db370004d886bac31a9153e526041c215edf2e
Luka <160645293+akulProgram@users.noreply.github.com>
2024-02-24 20:55:25 +0100

    Username-email
    simple email and username creator

---
+++ Username-email/Main.py
@@ -0,0 +1,9 @@
+firstname = input("Enter your first name:")
+lastname = input("Enter your last:")
+
+username = firstname + lastname
+
+print(username)
+
+email = username + "@gmail.com"
+print(email)
